2d8dff2f38b8ed80a836324efe4aa2fcb7430dd9
mohamed <mohamedqwe542@gmail.com>
2023-11-01 11:38:59 +0200

    a

--- src/components/UpdateCategoryComponents/UpdateSliderImages/UpdateSliderImages.js
+++ src/components/UpdateCategoryComponents/UpdateSliderImages/UpdateSliderImages.js
@@ -1,10 +1,17 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { MONGODB_URL } from "../../../envData";
 
 function UpdateSliderImages() {
+  const [mainImages, setMainImages] = useState();
+  const [transImages, setTransImages] = useState();
+  const [hajjImages, setHajjImages] = useState();
+  const [mainState, setMainState] = useState(true);
+  const [transState, setTransState] = useState(true);
+  const [hajjState, setHajjState] = useState(true);
+
   const convertToBase64 = (file) => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -24,14 +31,29 @@ function UpdateSliderImages() {
       // console.log(file[i]);
       // console.log(postImage);
       const base64 = await convertToBase64(file[i]);
-      allImages.push({ img_url: base64 });
+
+      if (allImages.length < file.length) {
+        allImages.push({ img_url: base64 });
+      } else {
+        await axios.post(`${MONGODB_URL}/mainSlider`, {
+          images: allImages,
+        });
+      }
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-      console.log(allImages);
-      await axios.post(`${MONGODB_URL}/mainSlider`, {
-        images: allImages,
-      });
+      if (allImages.length >= file.length) {
+        console.log("done");
+        await axios.post(`${MONGODB_URL}/mainSlider`, {
+          images: allImages,
+        });
+        break;
+      }
+      console.log(allImages, file.length);
+
+      // await axios.post(`${MONGODB_URL}/mainSlider`, {
+      //   images: allImages,
+      // });
     }
   };
   const handleUploadTransportasionSlider = async (e) => {
@@ -45,7 +67,7 @@ function UpdateSliderImages() {
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-       console.log(allImages);
+      console.log(allImages);
       await axios.post(`${MONGODB_URL}/transportationSlider`, {
         images: allImages,
       });
@@ -62,7 +84,7 @@ function UpdateSliderImages() {
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-       console.log(allImages);
+      console.log(allImages);
       await axios.post(`${MONGODB_URL}/hajjOmrahSlider`, {
         images: allImages,
       });
@@ -79,7 +101,7 @@ function UpdateSliderImages() {
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-       console.log(allImages);
+      console.log(allImages);
       await axios.patch(`${MONGODB_URL}/updateMainSlider`, {
         images: allImages,
       });
@@ -96,7 +118,7 @@ function UpdateSliderImages() {
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-       console.log(allImages);
+      console.log(allImages);
       await axios.patch(`${MONGODB_URL}/updateTransportationSlider`, {
         images: allImages,
       });
@@ -113,7 +135,7 @@ function UpdateSliderImages() {
       // setPostImage([{ ...postImage, myFile: base64 }]);
       // setData({ ...data, images: allImages });
       // console.log(data);
-       console.log(allImages);
+      console.log(allImages);
       await axios.patch(`${MONGODB_URL}/updateHajjOmrahSlider`, {
         images: allImages,
       });
@@ -137,6 +159,13 @@ function UpdateSliderImages() {
                 onChange={(e) => handleUploadMainSlider(e)}
               />
             </Form.Group>
+            <button
+              className="btn btn-warning"
+              disabled={mainState}
+              onClick={() => {}}
+            >
+              Upload
+            </button>
           </div>
           <div className="update-btn-box">
             <Form.Group controlId="formFileMultiple" className="mb-3">
