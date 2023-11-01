3ff82f75e23fea2c01f0ef2141743011f3b2cc4f
mohamed <mohamedqwe542@gmail.com>
2023-11-01 10:52:24 +0200

    o

--- src/components/UpdateCategoryComponents/UpdateSliderImages/UpdateSliderImages.js
+++ src/components/UpdateCategoryComponents/UpdateSliderImages/UpdateSliderImages.js
@@ -1,13 +1,116 @@
+import axios from 'axios';
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Form } from 'react-router-dom';
+import { MONGODB_URL } from '../../../envData';
 
 function UpdateSliderImages() {
-  return <Container>
-    <h1>Update Sliders Images</h1>
-    <div className='update-section'>
-      
-    </div>
-  </Container>;
+
+
+    const convertToBase64 = (file) => {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+      });
+    };
+    const handleUploadMainSlider = async (e) => {
+      const file = e.target.files;
+      const allImages = [];
+      for (let i = 0; i < file.length; i++) {
+        // console.log(file[i]);
+        // console.log(postImage);
+        const base64 = await convertToBase64(file[i]);
+        allImages.push({ img_url: base64 });
+        // setPostImage([{ ...postImage, myFile: base64 }]);
+        // setData({ ...data, images: allImages });
+        // console.log(data);
+       await axios.patch(`${MONGODB_URL}/updateMainSlider`,{images: allImages});
+      }
+    };
+    const handleUploadTransportasionSlider = async (e) => {
+      const file = e.target.files;
+      const allImages = [];
+      for (let i = 0; i < file.length; i++) {
+        // console.log(file[i]);
+        // console.log(postImage);
+        const base64 = await convertToBase64(file[i]);
+        allImages.push({ img_url: base64 });
+        // setPostImage([{ ...postImage, myFile: base64 }]);
+        // setData({ ...data, images: allImages });
+        // console.log(data);
+       await axios.patch(`${MONGODB_URL}/updateTransportationSlider`, {
+         images: allImages,
+       });
+      }
+    };
+    const handleUploadHajjOmrahSlider = async (e) => {
+      const file = e.target.files;
+      const allImages = [];
+      for (let i = 0; i < file.length; i++) {
+        // console.log(file[i]);
+        // console.log(postImage);
+        const base64 = await convertToBase64(file[i]);
+        allImages.push({ img_url: base64 });
+        // setPostImage([{ ...postImage, myFile: base64 }]);
+        // setData({ ...data, images: allImages });
+        // console.log(data);
+       await axios.patch(`${MONGODB_URL}/updateHajjOmrahSlider`, {
+         images: allImages,
+       });
+      }
+    };
+  return (
+    <Container>
+      <h1>Update Sliders Images</h1>
+      <div className="update-section">
+        <div className="update-btn-box">
+          <Form.Group controlId="formFileMultiple" className="mb-3">
+            <Form.Label>Upload Images Main Slider</Form.Label>
+            <input
+              multiple
+              type="file"
+              label="Image"
+              name="myFile"
+              accept=".jpeg, .png, .jpg"
+              onChange={(e) => handleUploadMainSlider(e)}
+            />
+          </Form.Group>
+        </div>
+        <div className="update-btn-box">
+          <Form.Group controlId="formFileMultiple" className="mb-3">
+            <Form.Label>Upload Images Transportation Slider</Form.Label>
+            <input
+              multiple
+              type="file"
+              label="Image"
+              name="myFile"
+              accept=".jpeg, .png, .jpg"
+              onChange={(e) => handleUploadTransportasionSlider(e)}
+            />
+          </Form.Group>
+        </div>
+        <div className="update-btn-box">
+          <Form.Group controlId="formFileMultiple" className="mb-3">
+            <Form.Label>Upload Images Hajj Omrah Slider</Form.Label>
+            <input
+              multiple
+              type="file"
+              label="Image"
+              name="myFile"
+              accept=".jpeg, .png, .jpg"
+              onChange={(e) => handleUploadHajjOmrahSlider(e)}
+            />
+          </Form.Group>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default UpdateSliderImages
